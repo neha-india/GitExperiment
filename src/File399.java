6a62be99a4008bb806ec420655eb2a46ee32d1ac
Neha Verma <neha.verma.pce@gmail.com>
2016-06-16 14:26:07 +0530

    I am a girl.

---
+++ src/File399.java
@@ -0,0 +1,13 @@
+1. The alias command is built into most of the shells on Linux systems and is a way of customizing a shell.
+ 
+2. Aliases are recognized only by the shell in which they are created, and apply only for the user who created it.
+ 
+ 3. The alias name and the replacement text can contain any valid shell input except for the equals sign (=).
+ 
+ 4. Aliases can be added to .bashrc (for bash shell) file to be made permanent.
+ 
+ 5. Remove aliases
+ 	
+- Aliases can be removed using the unalias command: unalias [-a] name(s)
+- Overwrite an existing alias by using the alias command to create a new alias with the same name.
+- A third way is to delete the alias from the appropriate configuration file using a text editor.
